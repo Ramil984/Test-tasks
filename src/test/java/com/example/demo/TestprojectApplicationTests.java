b6abf468ded817d60b48788466cced3319aa16f2
Ramil <80945245+Ramil984@users.noreply.github.com>
2021-03-19 23:44:09 +0300

    Update TestprojectApplicationTests.java

--- src/test/java/com/example/demo/TestprojectApplicationTests.java
+++ src/test/java/com/example/demo/TestprojectApplicationTests.java
@@ -3,12 +3,6 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.bind.annotation.RestController;
-@SpringBootTest
 @RestController
-class TestprojectApplicationTests {
-
-	@Get /users/v1
-	void contextLoads() {
-	}
-
-}
+Class Request
+	get
